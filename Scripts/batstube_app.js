955f7d651c4dc6fd38c20a41e16dea5c6a235d6b
philliha <hphillips1229@gmail.com>
2016-04-27 18:42:15 -0400

    General publish

--- Scripts/batstube_app.js
+++ Scripts/batstube_app.js
@@ -1,36 +1,15 @@
 ﻿var VidApp = angular.module('VidApp', ['ngRoute']);
 
-VidApp.controller('PathCtrl', ['$scope', function ($scope) {
-
-
-
-}]);
-
 VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-    // var paths = $location.search().paths;
-    // var titles = $location.search().titles;
-    // var network_path = false;
+
 
     $scope.model = {
         currentVideo: undefined
     };
-    
+
     $scope.angleChoice = {
-        mainOnly : false
+        mainOnly: true
     };
-
-
-    // if (paths) {
-    //     path = paths.split(';');
-    //     network_path = true;
-    // }       
-    // else
-    //     paths = ["/Images/1288A.mp4", "/Images/1289A.mp4", "/Images/1290A.mp4", "/Images/1291A.mp4"];
-    // 
-    // if (titles)
-    //     titles = titles.split(';');
-    // else
-    //     titles = ["Example Video 1", "Example Video 2", "Example Video 3", "Example Video 4"];
 
     $scope.videos = [];
     $scope.model.currentVideo = null;
@@ -38,53 +17,66 @@ VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope
     //console.log(videoPaths);
     //console.log(videoTitles);
 
+    // KeyDown Events
+    $(document).on("keydown", function (event) {
 
-    
+        if (event.which == 32) { // SPACE
+            event.preventDefault();
+            $scope.pause();
+        }
+        else if (event.which == 37 || event.which == 65) { // LEFT
+            event.preventDefault();
+            $scope.stepBack();
+        }
+        else if (event.which == 38 || event.which == 87) { // UP
+            event.preventDefault();
+            $scope.play();
+        }
+        else if (event.which == 39 || event.which == 68) { // RIGHT
+            event.preventDefault();
+            $scope.stepForward();
+        }
 
+        else if (event.which == 40 || event.which == 83) { // DOWN
+            event.preventDefault();
+            $scope.slow();
+        }
 
-//     for (var i = 0; i < paths.length; i++) {
-//         if (paths[i].replace(/ /g, '') != "") {
-//             if (network_path) {
-//                 vid_info = {
-//                     path: "/Content" + paths[i].replace(/ /g, '').replace(/"/, '').replace(/\\/g, "/").split("MAJORS")[1],
-//                     title: titles[i].trim()
-//                 }
-//             } else {
-//                 vid_info = {
-//                     path: paths[i],
-//                     title: titles[i].trim()
-//                 }
-//             }
-//             $scope.videos.push(vid_info);
-// 
-//         }            
-//     }
+    });
+
 
     $scope.setupVideos = function (x) {
         $scope.sentVideos = [];
         for (var i = 0; i < videoPaths.length; i++) {
             if (videoPaths[i] != "") {
                 //console.log($.trim(videoTitles[i]).length);
-                if(!$scope.angleChoice.mainOnly || $.trim(videoTitles[i]).length > 1) {
+                if (!$scope.angleChoice.mainOnly || $.trim(videoTitles[i]).length > 1) {
                     $scope.sentVideos.push(
                         {
-                            path: videoPaths[i].trim().replace(/\s+/g, "/"),
+                            path: videoPaths[i].trim().replace(/\s+/g, "/").replace("192.169.190.40", "vids6.sydexsports.net"),
                             title: videoTitles[i]
                         }
-                    );                    
+                    );
                 }
 
             }
-        };        
+        };
     };
 
     $scope.setupVideos();
     $scope.model.currentVideo = $scope.sentVideos[0];
 
-    videojs("main_vid", { "controls": true, "autoplay": false, "preload": "auto",  techOrder: ["html5", "flash"] }, function () {
+    videojs("main_vid", { "controls": true, "autoplay": false, "preload": "auto", techOrder: ["html5", "flash"] }, function () {
+        this.trigger("loadstart");
+
         this.src([{ type: "video/mp4", src: $scope.model.currentVideo.path }]);
+        this.errorDisplay.close();
+
         this.play();
-        
+
+
+
+
         this.on('ended', function () {
             $scope.$apply(function () {
                 var next_vid_index = ($scope.sentVideos.indexOf($scope.model.currentVideo) + 1) % $scope.sentVideos.length;
@@ -98,12 +90,10 @@ VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope
 
 
     $scope.setCurrentVideo = function (newVid) {
-
-
         $scope.model.currentVideo = newVid;
 
         videojs("main_vid").ready(function () {
-            
+
             var myvid = this;
             var currentRate = myvid.playbackRate();
             //console.log(currentRate);
@@ -111,12 +101,12 @@ VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope
 
             myvid.src([{ type: "video/mp4", src: newVid.path }]);
             myvid.addClass("embed-responsive-item");
-	
-	    myvid.defaultPlaybackRate = currentRate;
+
+            myvid.defaultPlaybackRate = currentRate;
             myvid.play();
             this.playbackRate(currentRate);
         });
-            
+
     };
 
     $scope.stepForward = function () {
@@ -133,8 +123,8 @@ VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope
     };
 
     $scope.stepBack = function () {
-        videojs("main_vid").ready(function () { 
-            var myvid = this;   
+        videojs("main_vid").ready(function () {
+            var myvid = this;
             var fps = 29;
             var frameLength = 1 / fps;
 
@@ -148,8 +138,8 @@ VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope
         videojs("main_vid").ready(function () {
             var myvid = this;
             this.defaultPlaybackRate = 1.0;
-	               myvid.play();
-	
+            myvid.play();
+
             myvid.playbackRate(1.0);
         });
     };
@@ -167,7 +157,11 @@ VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope
         videojs("main_vid").ready(function () {
             var myvid = this;
 
-            this.pause();
+            if (myvid.paused()) {
+                myvid.play();
+            } else {
+                myvid.pause();
+            }
         });
     };
 }]);
