d7a5ed060093c7f249d86597119dc2404d458e0a
pmanko <piotr.mankowski@gmail.com>
2016-04-26 21:18:24 -0400

    Red Sox update and fixes for batstube issues

--- Scripts/batstube_app.js
+++ Scripts/batstube_app.js
@@ -68,7 +68,7 @@ VidApp.controller('VideoCtrl', ['$scope', '$http', '$location', function ($scope
                 if(!$scope.angleChoice.mainOnly || $.trim(videoTitles[i]).length > 1) {
                     $scope.sentVideos.push(
                         {
-                            path: videoPaths[i].trim().replace(/\s+/g, "/").replace("192.169.250.162", "vids3.sydexsports.net"),
+                            path: videoPaths[i].trim().replace(/\s+/g, "/"),
                             title: videoTitles[i]
                         }
                     );                    
